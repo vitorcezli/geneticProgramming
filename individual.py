1575300f60a58000ce1bdd9e6a6de185f1635f3f
Vítor Cézar <vitorcezli@gmail.com>
2017-09-18 11:33:08 -0300

    Done with classification

--- individual.py
+++ individual.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from abc import ABC, abstractmethod
+import copy
 
 
 class individual(ABC):
@@ -10,28 +11,63 @@ class individual(ABC):
 		self.genotype = self.generate_genotype(size)
 		self.number_arguments = number_arguments
 
+
 	def get_number_arguments(self):
 		"Gets the number of arguments necessary"
 		return self.number_arguments
 
+
 	def get_genotype(self):
 		"Returns the individual's genotype"
 		return self.genotype
+
+
+	def classify(self, values):
+		"Classifies the data based on the genotype"
+		# gets the datum with the values
+		datum_and_values = self.get_datum_with_values(values)
+		return self.classify_datum_with_values(datum_and_values)
+
+
+	def get_datum_with_values_h(self, values, vi, g_list):
+		"Gets the datum with the values recursivelly"
+		for index in range(len(g_list)):
+			if g_list[index] == 'XX':
+				g_list[index] = values[vi]
+				vi += 1
+			elif type(g_list[index]) is list:
+				vi = self.get_datum_with_values_h(values, vi, g_list[index])
+		return vi
+
+
+	def get_datum_with_values(self, values):
+		"Gets the datum with the values"
+		if len(values) != self.get_number_arguments():
+			print("Error on values' length")
+			exit(1)
+
+		genotype_copy = copy.deepcopy(self.get_genotype())
+		self.get_datum_with_values_h(values, 0, genotype_copy)
+		return genotype_copy
+
+
+	@abstractmethod
+	def classify_datum_with_values(self, datum_and_values):
+		"Classifies the data based on the genotype using recursion"
+		pass
+
 
 	@abstractmethod
 	def generate_genotype(self, size):
 		"Generates the individual's genotype"
 		pass
 
-	@abstractmethod
-	def classify(self, data):
-		"Classifies the data based on the genotype"
-		pass
 
 	@abstractmethod
 	def mutate(self):
 		"Mutates the individual"
 		pass
+
 
 	@abstractmethod
 	def cross(self, other_individual):
--- individualKeijzer7.py
+++ individualKeijzer7.py
@@ -14,59 +14,27 @@ class individualKeijzer7(individual):
 		super().__init__(size, number_arguments)
 
 
-
 	def generate_genotype(self, size):
 		"Generates the individual's genotype"
 		return ['sum', ['log', 10, 'XX'], 3]
 
 
-
-	def get_datum_with_values_h(self, values, vi, g_list):
-		for index in range(len(g_list)):
-			if g_list[index] == 'XX':
-				g_list[index] = values[vi]
-				vi += 1
-			elif type(g_list[index]) is list:
-				vi = self.get_datum_with_values_h(values, vi, g_list[index])
-		return vi
-
-
-	def get_datum_with_values(self, values):
-		"Gets the datum with the values"
-		if len(values) != super().get_number_arguments():
-			print("Error on values' length")
-			exit(1)
-
-		genotype_copy = super().get_genotype()
-		self.get_datum_with_values_h(values, 0, genotype_copy)
-		return genotype_copy
-
-
-
-
-	def classify(self, values):
-		"Classifies the data based on the genotype"
-		# gets the datum with the values
-		datum_and_values = self.get_datum_with_values(values)
-		print(datum_and_values)
-		return classify_datum_with_values(datum_and_values)
-
-
 	def classify_datum_with_values(self, datum_and_values):
+		"Classifies the data based on the genotype using recursion"
 		# returns the same number if there isn't a function
 		if isinstance(datum_and_values, numbers.Number):
 			return datum_and_values
 
 		# classifies for log function
 		elif datum_and_values[0] == 'log':
-			base = self.classify(datum_and_values[1])
-			exponent = self.classify(datum_and_values[2])
+			base = self.classify_datum_with_values(datum_and_values[1])
+			exponent = self.classify_datum_with_values(datum_and_values[2])
 			return math.log(exponent, base)
 
 		# classifies for sum function
 		elif datum_and_values[0] == 'sum':
-			factor1 = self.classify(datum_and_values[1])
-			factor2 = self.classify(datum_and_values[2])
+			factor1 = self.classify_datum_with_values(datum_and_values[1])
+			factor2 = self.classify_datum_with_values(datum_and_values[2])
 			return factor1 + factor2
 
 		# an error has happened
@@ -76,18 +44,14 @@ class individualKeijzer7(individual):
 			exit(1)		
 		
 
-
 	def mutate(self):
 		"Mutates the individual"
 		pass
 
 
-
 	def cross(self, other_individual):
 		"Returns a new individual using crossover operation"
 		pass
-
-
 
 
 individual = individualKeijzer7(7, 1)
